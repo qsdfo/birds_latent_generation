6a2f44229941f7b44fe2ff3d0b9b1453518e373a
leo <crestel.leopoold@gmail.com>
2020-04-11 13:05:34 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='avgn',
+    packages=find_packages(),
+    version='0.2.0',
+    description='Latent models for visualizing, characterizing, and generating animal vocalizations',
+    author='Tim Sainburg',
+    license='MIT',
+)
