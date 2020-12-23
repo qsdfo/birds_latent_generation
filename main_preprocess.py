91d9baddc19ba65828b59d753f95f061c327af22
130 <crestel.leopold@gmail.com>
2020-12-23 19:09:35 +0100

    Custom db preprocessing

--- main_preprocess.py
+++ main_preprocess.py
@@ -7,25 +7,28 @@ from datetime import datetime
 
 from pathlib2 import Path
 
-from avgn.custom_parsing.bird_db import generate_json
+from avgn.custom_parsing.bird_db import generate_json, generate_json_custom
 from avgn.downloading.birdDB import openBirdDB_df
 from avgn.utils.paths import DATA_DIR
 
 
 def main():
-    song_db = openBirdDB_df()
-    DATASET_ID = 'bird-db'
+    # song_db = openBirdDB_df()
+    # DATASET_ID = 'bird-db'
+    # DT_ID = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    song_db = None
+    DATASET_ID = 'voizo'
     DT_ID = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     dataset_path = Path(f'{DATA_DIR}/raw/{DATASET_ID}')
     wavs = set((dataset_path).expanduser().glob('**/*.wav'))
 
     for wf in wavs:
-        try:
+        if song_db is not None:
             generate_json(wf, DT_ID, song_db)
-        except:
-            continue
-
+        else:
+            generate_json_custom(wf, DT_ID)
 
 if __name__ == '__main__':
     main()
